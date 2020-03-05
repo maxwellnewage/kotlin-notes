165c7484bdc4e03a10d80fa3f7d8bc29c92089fe
Maximiliano Burgos <maxib2007@gmail.com>
2020-03-04 23:51:29 -0300

    preparing add and search note functions

--- app/src/main/java/com/maxwell/notesapp/MainActivity.kt
+++ app/src/main/java/com/maxwell/notesapp/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.maxwell.notesapp
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.LinearLayout
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,5 +30,43 @@ class MainActivity : AppCompatActivity() {
         val adapter = NoteAdapter(noteList)
 
         rvNotes.adapter = adapter
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+
+            R.id.iAddNote -> {
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+
+        val searchView = menu?.findItem(R.id.iSearchNote)?.actionView as SearchView
+        val serviceManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        searchView.setSearchableInfo(serviceManager.getSearchableInfo(componentName))
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                searchNote(p0)
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun searchNote(term:String?){
+
     }
 }
