da2b733f93e1a4c739c8ec99bacac0924eac3415
Maximiliano Burgos <maxib2007@gmail.com>
2020-03-07 00:54:10 -0300

    finally i fixed the popup visualization

--- app/src/main/java/com/maxwell/notesapp/AddNoteDialogFragment.kt
+++ app/src/main/java/com/maxwell/notesapp/AddNoteDialogFragment.kt
@@ -1,24 +1,19 @@
 package com.maxwell.notesapp
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.add_note.view.*
 
 class AddNoteDialogFragment(val onNoteListener: OnNoteListener) : DialogFragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.add_note, container)
-    }
 
-    override fun onViewCreated(v: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(v, savedInstanceState)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+
+        val v: View = LayoutInflater.from(this.activity).inflate(R.layout.add_note, null, false)
 
         v.btAdd.setOnClickListener {
             val note = v.etAddNote.text.toString()
@@ -31,5 +26,9 @@ class AddNoteDialogFragment(val onNoteListener: OnNoteListener) : DialogFragment
         v.btCancel.setOnClickListener {
             dismiss()
         }
+
+        builder.setView(v)
+        return builder.create()
     }
+
 }
